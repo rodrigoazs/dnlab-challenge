e05adb02f2387c6edd7bb0f488e3f4f7ead0d58f
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2021-05-09 20:20:48 -0300

    Add docstrings to routes

--- api/models.py
+++ api/models.py
@@ -3,7 +3,6 @@ from bson import ObjectId
 
 
 class PyObjectId(ObjectId):
-
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
--- api/routes.py
+++ api/routes.py
@@ -16,6 +16,22 @@ async def view_urparts(
     part: Optional[str] = None,
     part_category: Optional[str] = None
 ):
+    """A route to retrieve machinery parts data
+    considering query strings.
+
+    Args:
+        manufacturer (str, optional): A manufacturer query. Defaults to None.
+        category (str, optional): A category query. Defaults to None.
+        model (str, optional): A model query. Defaults to None.
+        part (str, optional): A part query. Defaults to None.
+        part_category (str, optional): A part categoruy query. Defaults to None.
+
+    Raises:
+        HTTPException: Raises an Internal Server Error.
+
+    Returns:
+        List[Part]: A list of parts model.
+    """
     try:
         return action_view_urparts(
             manufacturer,
